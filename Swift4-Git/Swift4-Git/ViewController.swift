5f203e394bfc5c1dac037b7ad033fcbe446043ce
Apple Swift Classes <bobisoftsp@gmail.com>
2018-01-17 02:02:43 +0100

    Proyecto Vasio
    Proyecto Vasio

---
+++ Swift4-Git/Swift4-Git/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift4-Git
+//
+//  Created by Apple on 17/1/18.
+//  Copyright © 2018 TattooInkValladolid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
